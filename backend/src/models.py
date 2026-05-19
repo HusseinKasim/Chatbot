6101d426bfdad713af3b30592438619c747a0a8e
Hussein Kasim <husseinkasim20@yahoo.com>
2026-05-19 15:15:31 +0200

    Update database schema

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,20 +1,32 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
 from database import Base
 
 class Users(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, index=True)
-    last_name = Column(String, index=True)
-    email = Column(String, index=True)
-    password = Column(String, index=True)
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, index=True, unique=True)
+    password = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
 
 class Chats(Base):
     __tablename__ = 'chats'
     
-    id = Column(Integer, primary_key=True, index=True)
-    # ChatList 
-    chat = Column(String, index=True) # TEST
-    #user_id = Column(Integer, ForeignKey('users.id'))
+    id = Column(Integer, primary_key=True)
+    chat_title = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Messages(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True)
+    role = Column(String)
+    message_text = Column(Text)
+    chat_id = Column(Integer, ForeignKey('chats.id'), index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
