a5743003f3f27f4520e1f3a654e979887571a1d0
Hussein Kasim <husseinkasim20@yahoo.com>
2026-08-03 10:58:58 +0200

    Implement RAG System (Ingestion)

--- backend/src/rag/ingest.py
+++ backend/src/rag/ingest.py
@@ -1,8 +1,30 @@
-# Document (PDF) Ingestion:
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
+
+# Document (PDF) Ingestion
+def ingest_doc(uploadedFilePath: Path):
     # Load PDFs
-    # Parse PDFs
+    loader = PyPDFLoader(str(uploadedFilePath))
+    document = loader.load()
+
     # Split PDFs into chunks
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    document_chunks = splitter.split_documents(document)
+
     # Create embeddings
+    embeddings = OpenAIEmbeddings(
+        model='text-embedding-3-small',
+        api_key=OPENAI_API_KEY
+    )
+
     # Store vectors in pgvector
+
 
 
--- backend/src/routers/upload.py
+++ backend/src/routers/upload.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, File
 from pathlib import Path
+from src.rag.ingest import ingest_doc
 import shutil
 
 router = APIRouter(prefix='/api/upload')
@@ -7,12 +8,17 @@ router = APIRouter(prefix='/api/upload')
 UPLOAD_DIR = Path('uploads')
 
 @router.post('/')
-async def upload(pdfFile: UploadFile):
+async def upload(pdfFile: UploadFile = File(...)):
+    # Create uploads folder (if not already created)
+    UPLOAD_DIR.mkdir(exist_ok=True)
+    # TODO: Add subdirectory for each user's documents
+    
     # Save PDF
     file_path = UPLOAD_DIR / pdfFile.filename
     with open(file_path, 'wb') as result_file:
         shutil.copyfileobj(pdfFile.file, result_file)
 
     # Call ingest
+    ingest_doc(file_path)
 
     return {'response': 'success'}
