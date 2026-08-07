067a32d63395ed690ddd5b1bba657c9263e34954
Hussein Kasim <husseinkasim20@yahoo.com>
2026-08-07 22:41:46 +0200

    Implement separate folders for different user docs

--- backend/src/routers/upload.py
+++ backend/src/routers/upload.py
@@ -7,14 +7,13 @@ from src.dependencies import get_db, get_current_user
 
 router = APIRouter(prefix='/api/upload')
 
-UPLOAD_DIR = Path('uploads')
-
 @router.post('/')
 async def upload(pdfFile: UploadFile = File(...), db: Session = Depends(get_db), user = Depends(get_current_user)):
-    # Create uploads folder (if not already created)
-    UPLOAD_DIR.mkdir(exist_ok=True)
-    # TODO: Add subdirectory for each user's documents
     
+    # Create uploads folder (if not already created)
+    UPLOAD_DIR = Path('uploads') / user['sub']
+    UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
     # Save PDF
     file_path = UPLOAD_DIR / pdfFile.filename
     with open(file_path, 'wb') as result_file:
