a570ca9db0d6cd298b49b3ce12522b6bc99f70f9
Hussein Kasim <husseinkasim20@yahoo.com>
2026-08-21 19:55:01 +0300

    Update textarea background color

--- frontend/src/components/UserTextArea.jsx
+++ frontend/src/components/UserTextArea.jsx
@@ -3,7 +3,7 @@ import { Textarea } from "@/components/ui/textarea";
 export default function UserTextArea({value, onChange, onKeyDown, isLoggedIn}) {
   return(
     <>
-    <Textarea className={`w-full h-[55px] py-[17px] pr-[50px] ${ isLoggedIn ? 'pl-[60px]' : 'pl-[15px]' } md:text-lg rounded-[20px] border border-[#FFEEDD] resize-none overflow-hidden`} value={value} onChange={onChange} onKeyDown={onKeyDown} placeholder='Ask a question...' />
+    <Textarea className={`w-full h-[55px] py-[17px] pr-[50px] ${ isLoggedIn ? 'pl-[60px]' : 'pl-[15px]' } md:text-lg rounded-[20px] border border-[#FFEEDD] resize-none overflow-hidden bg-[#1e1e20]`} value={value} onChange={onChange} onKeyDown={onKeyDown} placeholder='Ask a question...' />
     </>
   )
 }
