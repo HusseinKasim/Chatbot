b09679fc920e1f8f54e5be13d90d8d5a77fae671
Hussein Kasim <husseinkasim20@yahoo.com>
2026-08-28 17:16:14 +0300

    Cleanup test_prompt.py

--- backend/src/tests/test_prompt.py
+++ backend/src/tests/test_prompt.py
@@ -10,7 +10,7 @@ client = TestClient(app)
 @patch('src.routers.prompt.client.chat.completions.create')
 def test_guest_user_prompt_response(mock_groq):
     test_messages = [{
-         'role': 'assistant',
+        'role': 'assistant',
         'content': 'Hello! How can I help you today?'
     },
     {
@@ -41,8 +41,8 @@ def test_guest_user_prompt_empty(mock_groq):
     test_messages = [{
             'role': 'assistant',
             'content': 'Hello! How can I help you today?'
-            },
-            {
+        },
+        {
             'role': 'user',
             'content': ''
         }]
@@ -58,65 +58,68 @@ def test_guest_user_prompt_empty(mock_groq):
 # Test case: Test_Logged_In_User_Prompt_Response
 @patch('src.routers.prompt.client.chat.completions.create')
 def test_logged_in_user_prompt_response_new_chat(mock_groq, db, db_user_auth):
-    prompt = 'What is a test?'
-    chatID = 0
+    try:
+        prompt = 'What is a test?'
+        chatID = 0
 
-    app.dependency_overrides[get_db] = lambda: db
-    app.dependency_overrides[get_current_user] = lambda: db_user_auth
+        app.dependency_overrides[get_db] = lambda: db
+        app.dependency_overrides[get_current_user] = lambda: db_user_auth
 
-    mock_groq.return_value.choices[0].message.content = 'A test is a planned procedure or set of actions executed to evaluate whether a software application, hardware component, or system functions correctly, securely, and efficiently.'
-    response = client.post('/api/prompt/user', json={'prompt': prompt, 'chatID': chatID})
-    assert response.status_code == 200
-    
-    data = response.json()
-    assert data['response'] is not None
-    assert data['response'] == mock_groq.return_value.choices[0].message.content
+        mock_groq.return_value.choices[0].message.content = 'A test is a planned procedure or set of actions executed to evaluate whether a software application, hardware component, or system functions correctly, securely, and efficiently.'
+        response = client.post('/api/prompt/user', json={'prompt': prompt, 'chatID': chatID})
+        assert response.status_code == 200
+        
+        data = response.json()
+        assert data['response'] is not None
+        assert data['response'] == mock_groq.return_value.choices[0].message.content
 
-    # TODO: ASSERT NEWLY GENERATED CHATID IN DB IS NOT 0
+        # TODO: ASSERT NEWLY GENERATED CHATID IN DB IS NOT 0
 
-    # TODO: ASSERT THAT THE PROMPT MESSAGE WAS STORED WITH THE NEWLY GENERATED CHATID IN DB
-
-    app.dependency_overrides.clear()
+        # TODO: ASSERT THAT THE PROMPT MESSAGE WAS STORED WITH THE NEWLY GENERATED CHATID IN DB
+    finally:
+        app.dependency_overrides.clear()
 
 
 # Test case: Test_Logged_In_User_Prompt_Response_Existing_Chat
 @patch('src.routers.prompt.client.chat.completions.create')
 def test_logged_in_user_prompt_response_existing_chat(mock_groq, db, db_user_auth, db_user_chat):
-    prompt = 'What is a test?'
-    chatID = db_user_chat.id
+    try:
+        prompt = 'What is a test?'
+        chatID = db_user_chat.id
 
-    app.dependency_overrides[get_db] = lambda: db
-    app.dependency_overrides[get_current_user] = lambda: db_user_auth
+        app.dependency_overrides[get_db] = lambda: db
+        app.dependency_overrides[get_current_user] = lambda: db_user_auth
 
-    mock_groq.return_value.choices[0].message.content = 'A test is a planned procedure or set of actions executed to evaluate whether a software application, hardware component, or system functions correctly, securely, and efficiently.'
-    response = client.post('/api/prompt/user', json={'prompt': prompt, 'chatID': chatID})
-    assert response.status_code == 200
-    
-    data = response.json()
-    assert data['response'] is not None
-    assert data['response'] == mock_groq.return_value.choices[0].message.content
+        mock_groq.return_value.choices[0].message.content = 'A test is a planned procedure or set of actions executed to evaluate whether a software application, hardware component, or system functions correctly, securely, and efficiently.'
+        response = client.post('/api/prompt/user', json={'prompt': prompt, 'chatID': chatID})
+        assert response.status_code == 200
+        
+        data = response.json()
+        assert data['response'] is not None
+        assert data['response'] == mock_groq.return_value.choices[0].message.content
 
-    # TODO: ASSERT CHATID IN DB IS EQUAL TO db_user_chat.id
+        # TODO: ASSERT CHATID IN DB IS EQUAL TO db_user_chat.id
 
-    # TODO: ASSERT THAT THE PROMPT MESSAGE WAS STORED IN THE CHATID IN DB
-
-    app.dependency_overrides.clear()
+        # TODO: ASSERT THAT THE PROMPT MESSAGE WAS STORED IN THE CHATID IN DB
+    finally:
+        app.dependency_overrides.clear()
 
 
 # Test case: Test_Logged_In_User_Prompt_Empty
 @patch('src.routers.prompt.client.chat.completions.create')
 def test_logged_in_user_prompt_empty(mock_groq, db, db_user_auth):
-    prompt = ''
-    chatID = 0
+    try:
+        prompt = ''
+        chatID = 0
 
-    app.dependency_overrides[get_db] = lambda: db
-    app.dependency_overrides[get_current_user] = lambda: db_user_auth
+        app.dependency_overrides[get_db] = lambda: db
+        app.dependency_overrides[get_current_user] = lambda: db_user_auth
 
-    mock_groq.return_value.choices[0].message.content = "I've processed an empty prompt!"
-    response = client.post('/api/prompt/user', json={'prompt': prompt, 'chatID': chatID})
-    assert response.status_code == 400
-    
-    data = response.json()
-    assert data['detail'] == 'Prompt cannot be empty'
-
-    app.dependency_overrides.clear()
+        mock_groq.return_value.choices[0].message.content = "I've processed an empty prompt!"
+        response = client.post('/api/prompt/user', json={'prompt': prompt, 'chatID': chatID})
+        assert response.status_code == 400
+        
+        data = response.json()
+        assert data['detail'] == 'Prompt cannot be empty'
+    finally:
+        app.dependency_overrides.clear()
