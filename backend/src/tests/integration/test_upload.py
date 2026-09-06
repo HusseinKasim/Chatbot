6502c98573929ea12d4d621057651b77b1bede4b
Hussein Kasim <husseinkasim20@yahoo.com>
2026-09-06 14:14:13 +0300

    Add test_ingest_doc_dev_environment

--- backend/src/tests/integration/test_upload.py
+++ backend/src/tests/integration/test_upload.py
@@ -7,7 +7,7 @@ from src.rag.ingest import ingest_doc
 
 client = TestClient(app)
 
-# Test case: Test_Upload_Development_Environment
+# Test case: Test_Upload_Dev_Environment
 @patch('src.routers.upload.ingest_doc')
 def test_upload_dev_environment(mock_ingest_doc, db, db_user_auth, sample_pdf_file, monkeypatch):
     monkeypatch.setenv('ENVIRONMENT', 'development')
@@ -21,5 +21,25 @@ def test_upload_dev_environment(mock_ingest_doc, db, db_user_auth, sample_pdf_fi
 
         # Assert successful response
         assert response.status_code == 200
+    finally:
+        app.dependency_overrides.clear()
+
+
+# Test case: Test_Ingest_Doc_Dev_Env
+def test_ingest_doc_dev_environment(db, db_user_auth, sample_pdf_file):
+    app.dependency_overrides[get_db] = lambda: db
+    app.dependency_overrides[get_current_user] = lambda: db_user_auth
+
+    try:
+        results = ingest_doc(sample_pdf_file, db, db_user_auth, s3_key=None)
+        data = results.json()
+
+        # Assert document added to db
+        new_db_document = db.query(models.Documents).filter(models.Documents.user_id == db_user_auth['sub']).order_by(models.Documents.id.desc).first()
+        assert data['document_added'] == new_db_document.id
+
+        # Assert document chunks added to db
+        new_db_document_chunks = db.query(models.Chunks).join(models.Documents).filter(models.Documents.user_id == db_user_auth['sub'], models.Chunks.document_id == new_db_document.id).all()
+        assert data['chunks'] == len(new_db_document_chunks)
     finally:
         app.dependency_overrides.clear()
