8e1196377f541c6212618940e9a3204281540971
Hussein Kasim <husseinkasim20@yahoo.com>
2026-09-13 15:44:20 +0300

    Add test_user_documents_fetch_empty and update test_user_documents_fetch

--- backend/src/tests/integration/test_documents.py
+++ backend/src/tests/integration/test_documents.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from src.app import app
-from src.dependencies import get_db, get_current_user
+from src.dependencies import get_db, get_current_user_optional
 from src import models
 
 client = TestClient(app)
@@ -8,7 +8,7 @@ client = TestClient(app)
 # Test case: Test_User_Documents_Fetch
 def test_user_documents_fetch(db, db_user_auth, db_user_document):
     app.dependency_overrides[get_db] = lambda: db
-    app.dependency_overrides[get_current_user] = lambda: db_user_auth
+    app.dependency_overrides[get_current_user_optional] = lambda: db_user_auth
 
     response = client.get('/api/documents/')
     data = response.json()
@@ -21,3 +21,19 @@ def test_user_documents_fetch(db, db_user_auth, db_user_document):
 
     # Assert db document is correctly fetched
     assert data['documents'][0]['id'] == db_user_document.id
+
+
+# Test case: Test_User_Documents_Fetch_Empty
+def test_user_documents_fetch_empty(db, db_user_document):
+    app.dependency_overrides[get_db] = lambda: db
+    app.dependency_overrides[get_current_user_optional] = None
+
+    response = client.get('/api/documents/')
+
+    # Assert successful response
+    assert response.status_code == 401 
+
+    data = response.json()
+    
+    # Assert db document is correctly fetched
+    assert data['detail'] == 'Invalid user'
