eeceb463a2094ac3442266cf24ca6015909226ff
Hussein Kasim <husseinkasim20@yahoo.com>
2026-09-13 15:43:15 +0300

    Update documents fetch endpoint

--- backend/src/routers/documents.py
+++ backend/src/routers/documents.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
-from src.dependencies import get_current_user, get_db
+from src.dependencies import get_current_user_optional, get_db
 from src import models
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix='/api/documents')
 
 @router.get('/')
-async def get_user_documents(user=Depends(get_current_user), db: Session = Depends(get_db)):
+async def get_user_documents(user=Depends(get_current_user_optional), db: Session = Depends(get_db)):
     if not user:
         raise HTTPException(status_code=401, detail='Invalid user')
     
